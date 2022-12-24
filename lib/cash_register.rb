c385fc828a6473e8ea2dbcafe3ee0c86cd9556c5
frank <franksavena@gmail.com>
2022-12-23 18:33:08 -0600

    commit tree 0863758e7eebc0085847459608b46776cc8fc926

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister 
+    attr_accessor :discount, :total, :items, :prev_item
+
+    
+
+    def initialize(discount = 0) 
+        @discount=discount
+        @total = 0
+        @items = []
+    end
+
+    # def total
+    #     @total = total
+    # end
+
+    def add_item(title, price, quantity = 1)
+        @total += (price * quantity) 
+        self.prev_item = (price * quantity)
+        @total 
+        quantity.times { @items << title }
+
+    end
+
+    def apply_discount
+        @total -= (self.total * (self.discount / 100) )
+     
+    end
+
+    def void_last_transaction
+        @total -= self.prev_item
+       
+    end
+
+
+  
+end
+binding.pry
